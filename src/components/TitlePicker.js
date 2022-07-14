2043d87622be46767cc45bf76d5f00436259d1f9
Josh Petersen <jdpetersen92@gmail.com>
2022-07-14 09:23:45 -0700

    further styling to the results cards and started to create error handeling for unfound shows

--- src/components/TitlePicker.js
+++ src/components/TitlePicker.js
@@ -9,17 +9,26 @@ const TitlePicker = ({ searchArr, setSearched, setTitleId, setTitle, setResults 
         setResults(true);
     }
 
-    return (
-        <div className="titlePicker">
-            {
-                searchArr.map((title) => {
-                    return (
-                        <button id={title.id} key={title.id} onClick={handleClick}>{title.name}</button>
-                    )
-                })
-            }
-        </div>
-    )
+    if (searchArr.length === 0) {
+        return (
+            <div className="nores">
+                <h3>Sorry this movie or show title was not found. Double check spelling or try a new title.</h3>
+            </div>
+        )
+    } else {
+        return (
+            <div className="titlePicker">
+                {
+                    searchArr.map((title) => {
+                        return (
+                            <button id={title.id} key={title.id} onClick={handleClick}>{title.name}</button>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+
 }
 
 export default TitlePicker
